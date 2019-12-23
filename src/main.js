bb4445ff476e5a048e7cd6d52aaeb86d83c20d92
noresources <dennis.petta@gmail.com>
2019-12-24 00:03:23 +0200

    vuetify options cleanup

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 import '~/assets/style/index.scss'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+
 // Import default layout so we don't need to import it to every page
 import DefaultLayout from '~/layouts/Default.vue'
 
@@ -11,38 +12,24 @@ export default function (Vue, { router, appOptions, head, isClient }) {
   router.options.linkActiveClass = 'is-active';
   head.link.push({
     rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css?family=Roboto+Noto+Sans:300,400,500,700,400italic|Material+Icons'
-  })
+    href: 'https://fonts.googleapis.com/css?family=Montserrat+Roboto+Noto+Sans:300,400,500,700,400italic|Material+Icons'
+  });
   head.link.push({
     rel: 'stylesheet',
     href: 'https://use.fontawesome.com/releases/v5.7.0/css/all.css'
-  })
+  });
   head.meta.push({
     name: 'keywords',
     content: 'dennis, dionisis,pettas,cv,ceid'
-  })
-  const opts = { icons: {
-    
-    theme: {
-      themes: {
-        light: {
-          primary: '#00000',
-          secondary: '#b0bec5',
-          anchor: '#8c9eff',
-        },
-      },
-    },
-    iconfont: 'mdi',
-    
-    
-  }, 
-  }// opts includes, vuetify themes, icons, etc.
-  
+  });
+  head.script.push({src: '/hotjar.js'});
+
+  const opts = {};
+
   Vue.use(Vuetify);
   appOptions.vuetify = new Vuetify(opts)
 
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout);
-  head.script.push({src: '/hotjar.js'});
 }
 
